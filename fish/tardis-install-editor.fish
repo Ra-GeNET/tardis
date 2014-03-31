2df4fc40a14d41d0e7bf9a901920ee801e0da3c4
Ramon Gebben <github@ra-ge.net>
2014-03-31 16:19:31 +0200

    Update install-editor
    Still need repo for editor

--- fish/tardis-install-editor.fish
+++ fish/tardis-install-editor.fish
@@ -1,8 +1,8 @@
-function ds-install-editor -d "Install DevStar Editor"
-    cd /devstar/sites/
-    git clone ssh://devstar.ra-ge.net/devstar/sites/editor
+function ds-install-editor -d "Install tardis Editor"
+    cd /tardis/sites/
+    git clone ssh://devstar.ra-ge.net/tardis/sites/editor
     chmod -R g+w editor
-    cd /devstar/sites/editor/
+    cd /tardis/sites/editor/
     bundle install
     rake db:migrate
     rake db:data:load_dir
