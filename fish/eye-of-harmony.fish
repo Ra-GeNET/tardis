34de6a1ae2d2dfe4b4c6567ba861fa9263c3c5eb
Ramon Gebben <github@ra-ge.net>
2014-03-31 17:24:17 +0200

    Update eye-of-harmony

---
+++ fish/eye-of-harmony.fish
@@ -0,0 +1,37 @@
+function eye-of-harmony -d "Install core system"
+  crow notice 'Installing RVM'
+  tardis-install-rvm
+  
+  crow notice 'Installing Ngnix'
+  tardis-install-ngnix
+  
+  crow notice 'Installing Rails'
+  tardis-install-rails
+
+  crow notice 'Installing MariaDB'
+  tardis-install-mariadb
+  
+  crow notice 'Installing Diakonos'
+  tardis-install-diakonos
+  
+  crow notice 'Installing PHP'
+  tardis-install-php
+  
+  crow notice 'Installing PHP My Admin'
+  tardis-install-phpmyadmin
+  
+  crow notice 'Restarting Websever'
+  rweb
+  
+  crow notice 'Installing Node packages'
+  tardis-install-npms
+  
+  crow notice 'Done building The Eye of Harmony'
+  
+end
+  
+  
+  
+  
+  
+  
