ec602203f3c72846166cf75720268f73f7fbde35
Ramon Gebben <github@ra-ge.net>
2014-03-31 16:30:10 +0200

    Replace devstar => tardis

--- fish/tardis-install-wiki.fish
+++ fish/tardis-install-wiki.fish
@@ -1,16 +1,16 @@
-function ds-install-wiki -d "Fetch wiki-docs from github"
-   crow notice "Setting up wiki folder at /devstar/wiki"
-   mkdir -p /devstar/wiki
-   cd /devstar/wiki
+function tardis-install-wiki -d "Fetch wiki-docs from github"
+   crow notice "Setting up wiki folder at /tardis/wiki"
+   mkdir -p /tardis/wiki
+   cd /tardis/wiki
    if test -d ".git"
      crow notice "Pulling changes of the wiki from github"
      git pull
    else
      crow notice "Cloning wiki from github"
-     git clone https://github.com/RamonGebben/devstar.wiki.git . 
+     git clone https://github.com/RamonGebben/tardis.wiki.git . 
    end
-   mkdir -p /devstar/source/mad
-   cd /devstar/source/mad
+   mkdir -p /tardis/source/mad
+   cd /tardis/source/mad
    if test -d ".git" 
      crow notice "Pulling changes of mad from github"
      git pull
@@ -21,5 +21,5 @@ function ds-install-wiki -d "Fetch wiki-docs from github"
    crow notice "Installing mad (markdown document viewer)"
    sudo make install
    crow success "Wiki is ready! Usage: wiki <title>"
-   cd /devstar
+   cd /tardis
 end
