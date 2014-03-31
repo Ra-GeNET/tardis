02b1e1cb84ec8e7e6a4b6f17ab1caf21c9c34e35
Ramon Gebben <github@ra-ge.net>
2014-03-31 16:32:59 +0200

    Replace devstar => tardis

--- fish/tardis-user.fish
+++ fish/tardis-user.fish
@@ -1,4 +1,4 @@
-function ds-user -a name -d "Adds a user to this system"
+function tardis-user -a name -d "Adds a user to this system"
   switch "$name"
     case ''
       crow error "Usage: ds-user <username>"
@@ -9,16 +9,16 @@ function ds-user -a name -d "Adds a user to this system"
 
       if test -d /home/$name 
 
-        crow notice "Setting default group to 'devstar'"
-        sudo usermod -g devstar $name  
+        crow notice "Setting default group to 'tardis'"
+        sudo usermod -g tardis $name  
  
         crow notice "Hooking up byobu config"      
         sudo mkdir "/home/$name/.byobu"
         sudo touch "/home/$name/.byobu/.tmux.conf"
         sudo chmod 777 "/home/$name/.byobu"
         sudo chmod 777 "/home/$name/.byobu/.tmux.conf"
-        echo "source /devstar/config/byobu" > "/home/$name/.byobu/.tmux.conf" 
-        sudo chown -R $name:devstar "/home/$name/.byobu"
+        echo "source /tardis/config/byobu" > "/home/$name/.byobu/.tmux.conf" 
+        sudo chown -R $name:tardis "/home/$name/.byobu"
         sudo chmod 700 "/home/$name/.byobu"
         sudo chmod 700 "/home/$name/.byobu/.tmux.conf"
 
@@ -28,14 +28,14 @@ function ds-user -a name -d "Adds a user to this system"
 
         crow notice "Hooking up diakonos config"
         sudo mkdir -p "/home/$name/.diakonos"
-        sudo cp  /devstar/config/diakonos.conf "/home/$name/.diakonos/diakonos.conf"
+        sudo cp  /tardis/config/diakonos.conf "/home/$name/.diakonos/diakonos.conf"
 
         crow notice "Make git remember your credentials"
         sudo 'su -c "git config --global credential.helper cache" -s /bin/sh $name'
 
         crow success "User $name succesfully added"
-        mkdir -p /devstar/flags
-        touch /devstar/flags/first-user
+        mkdir -p /tardis/flags
+        touch /tardis/flags/first-user
 
      else
 
